70de63901bdf782999f17f550b029594cbb73aca
syhrlanwr <syhrlanwr@gmail.com>
2022-04-15 13:16:44 +0700

    add Mail entity

---
+++ src/main/java/id/ac/uin/forgotpassword/entity/Mail.java
@@ -0,0 +1,13 @@
+package id.ac.uin.forgotpassword.entity;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class Mail {
+    private String from;
+    private String to;
+    private String subject;
+    private Map<String, Object> model;
+}
