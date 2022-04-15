0135b309f176f2acfb7918beaa223e6416e02959
syhrlanwr <syhrlanwr@gmail.com>
2022-04-15 12:53:38 +0700

    first commit

---
+++ src/main/java/id/ac/uin/forgotpassword/ForgotpasswordApplication.java
@@ -0,0 +1,13 @@
+package id.ac.uin.forgotpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgotpasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgotpasswordApplication.class, args);
+	}
+
+}
